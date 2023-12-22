c988f6282bbb33166ab98426a974aa1ae6f2d13e
areebaalam <145709020+areebaalam@users.noreply.github.com>
2023-12-22 10:51:55 +0500

    factory method

---
+++ factorymethod/factorymethod/src/factorymethod/Factorymethod.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factorymethod;
+
+/**
+ *
+ * @author sp21-bse-072
+ */
+public class Factorymethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
